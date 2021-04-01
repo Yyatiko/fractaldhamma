4a5697f51ef3276c31387def8cdbe0b2f532e847
Yyatiko <54907189+Yyatiko@users.noreply.github.com>
2021-04-01 09:45:35 +0100

    mirror from external

---
+++ lib/tpl/fractal10/user/navChildren.php
@@ -0,0 +1,9 @@
+<?php $navChildren=array(
+'./data/pages/4nobletruths.txt'=>'
+',
+
+'./data/pages/start.txt'=>'
+<a href="doku.php?id=4nobletruths">4 Noble Truths</a>
+',
+
+) ?>
---
+++ lib/tpl/fractal10/user/navParent.php
@@ -0,0 +1,9 @@
+<?php $navParent=array(
+'./data/pages/4nobletruths.txt'=>'
+<a href="doku.php?id=start">Fractal Dhamma</a>
+<br/><br/>',
+
+'./data/pages/start.txt'=>'
+',
+
+) ?>
